63adc7e673ec02b526f0ef790f17ce1905d6eb1c
Marcos Piemontez <marcos.piemontez1@gmail.com>
2022-02-09 22:29:21 -0300

    ✨ NEW FEATURE: Adicionado função para salvar as informações do "todo"

--- App.js
+++ App.js
@@ -1,39 +1,78 @@
-import React, { useState } from 'react';
-import {  View, Text, SafeAreaView, StatusBar, TouchableOpacity, FlatList, Modal, TextInput } from 'react-native';
-import styles from './style'
-import { Ionicons } from '@expo/vector-icons'
-import TaskList from './src/components/TaskList/'
-import  *  as  Animatable  from  'react-native-animatable'
+import React, { useState, useCallback, useEffect } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  TextInput,
+} from "react-native";
+import styles from "./style";
+import { Ionicons } from "@expo/vector-icons";
+import TaskList from "./src/components/TaskList/";
+import * as Animatable from "react-native-animatable";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const AnimateBtn = Animatable.createAnimatableComponent(TouchableOpacity)
+const AnimateBtn = Animatable.createAnimatableComponent(TouchableOpacity);
 
 export default function App() {
   const [task, setTask] = useState([]);
   const [open, setOpen] = useState(false);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
+
+  // buscando todas as tarefas ao iniciar o app
+  useEffect(() => {
+    async function loadTaskList() {
+      const taskStorage = await AsyncStorage.getItem('@task');
+      if(taskStorage) {
+        setTask(JSON.parse(taskStorage));
+      }
+    }
+    loadTaskList();
+  }, []);
+
+  // Salvando caso tenha alguma tarefa alterada
+  useEffect(() => {
+    async function saveTasks() {
+      await AsyncStorage.setItem('@task', JSON.stringify(task));
+    }
+
+    saveTasks();
+
+  }, [task]);
+
+
+  { /* no rn, quando deixamos um array vazio assim, quer dizer que essa função deve ser chamada assim que a tela carregar! ou seja, seria um "mounted" */ }
 
   function handleAdd() {
-    if(input === '') return;
+    if (input === "") return;
 
     const data = {
       key: input,
-      task: input
+      task: input,
     };
 
     setTask([...task, data]);
     setOpen(false);
-    setInput('');
+    setInput("");
   }
+
+  const handleDelete = useCallback((data) => {
+    const find = task.filter((r) => r.key !== data.key);
+    setTask(find);
+  });
 
   return (
     <SafeAreaView style={styles.mainContainer}>
-      <StatusBar backgroundColor='#171d31' barStyle="light-content" />
+      <StatusBar backgroundColor="#171d31" barStyle="light-content" />
 
       <View style={styles.content}>
         <Text style={styles.title}> Minhas tarefas </Text>
       </View>
 
-       {/*propriedades do 'flatlist' showsHorizontalScrollIndicator = remove a barra de rolagem mas não perde o scroll
+      {/*propriedades do 'flatlist' showsHorizontalScrollIndicator = remove a barra de rolagem mas não perde o scroll
           data -> é as 'options da lista' no renderItem ({ item }) essa forma de importação pega o item que esta sendo passando no extractor
           o keyExtrator captura o index da lista
        */}
@@ -42,7 +81,9 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         data={task}
         keyExtractor={(item) => String(item.key)}
-        renderItem={({ item }) => <TaskList data={item} />}
+        renderItem={({ item }) => (
+          <TaskList data={item} handleDelete={handleDelete} />
+        )}
       />
 
       <View style={styles.modalHeader}>
@@ -50,29 +91,35 @@ export default function App() {
           <SafeAreaView style={styles.modal}>
             <View>
               <TouchableOpacity onPress={() => setOpen(false)}>
-                <Ionicons style={{marginLeft: 5, marginRight: 5}} name="md-arrow-back" size={40} color="#fff" />
+                <Ionicons
+                  style={{ marginLeft: 5, marginRight: 5 }}
+                  name="md-arrow-back"
+                  size={40}
+                  color="#fff"
+                />
               </TouchableOpacity>
               <Text style={styles.modalTitle}> Nova Tarefa </Text>
             </View>
 
-            <Animatable.View animation="fadeInUp" useNativeDriver style={styles.modalBody}>
+            <Animatable.View
+              animation="fadeInUp"
+              useNativeDriver
+              style={styles.modalBody}
+            >
               <TextInput
                 multiline={true}
                 placeholderTextColor="#747474"
                 autoCorrect={false}
-                placeholder='O que preciso fazer hoje?'
+                placeholder="O que preciso fazer hoje?"
                 style={styles.modalInput}
                 value={input}
                 onChangeText={(texto) => setInput(texto)}
               />
 
               <TouchableOpacity style={styles.handleAdd} onPress={handleAdd}>
-                <Text style={styles.handleText}>
-                  Cadastrar
-                </Text>
+                <Text style={styles.handleText}>Cadastrar</Text>
               </TouchableOpacity>
             </Animatable.View>
-
           </SafeAreaView>
         </Modal>
       </View>
@@ -82,11 +129,10 @@ export default function App() {
         animation="bounceIn"
         duration={1500}
         style={styles.fab}
-        onPress={() => setOpen(true) }
+        onPress={() => setOpen(true)}
       >
         <Ionicons name="ios-add" size={35} color="#fff" />
       </AnimateBtn>
-
     </SafeAreaView>
   );
 }
--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -4,14 +4,14 @@ import styles from './style'
 import { Ionicons } from '@expo/vector-icons'
 import * as Animation from 'react-native-animatable'
 
-export default function TaskList({ data }) {
+export default function TaskList({ data, handleDelete }) {
   return(
     <Animation.View
       style={styles.container}
       animation="bounceIn"
       useNativeDriver
     >
-      <TouchableOpacity>
+      <TouchableOpacity onPress={ () => handleDelete(data) }>
         <Ionicons name="md-checkmark-circle" size={30} color="#000" />
       </TouchableOpacity>
       <View>
